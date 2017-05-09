aa64793b68572014a42e801534bbb626c1a65ec2
Riley Thompson <rileyt_2000@hotmail.com>
2017-05-09 15:18:23 -0600

    add home controller fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,4 @@
 class HomeController < ApplicationController
   def index
-    RedditService.new.subs(current_user.token)
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,6 @@ class SessionsController < ApplicationController
   def create
     if user = User.from_omniauth(request.env["omniauth.auth"])
        session[:user_id] = user.id
-       #when user authenticates it finds by uid, unless updated_at > hour
-       #if that is true then we make a call to reddit API to update the token
     end
     redirect_to root_path
   end
